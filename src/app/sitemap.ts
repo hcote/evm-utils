88f9ff6913f786678340dc4ffe22fc778c06169c
Hunter Cote <huntercote2@gmail.com>
2025-05-05 15:32:54 -0400

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,23 +1,21 @@
 import { type MetadataRoute } from 'next';
 
-export async function GET(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://evmutils.com';
+const baseUrl = 'https://evmutils.com';
 
-  const paths = [
-    '',
-    '/unit-converter',
-    '/pk-generator',
-    '/tx-decoder',
-    '/nft',
-    '/ens',
-    '/compute-address',
-    '/data-decoder',
-  ];
+const paths = [
+  '',
+  '/unit-converter',
+  '/pk-generator',
+  '/tx-decoder',
+  '/nft',
+  '/ens',
+  '/compute-address',
+  '/data-decoder',
+];
 
-  const pages = paths.map((path) => ({
-    url: `${baseUrl}${path}`,
-    lastModified: new Date().toISOString(),
-  }));
+const sitemap: MetadataRoute.Sitemap = paths.map((path) => ({
+  url: `${baseUrl}${path}`,
+  lastModified: new Date().toISOString(),
+}));
 
-  return pages;
-}
+export default sitemap;
